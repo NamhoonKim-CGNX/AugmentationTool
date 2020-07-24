5b830d46aec1b429997c16b668e39b4ba2d466cd
NATICK-NT\nakim <Thomas.kim@cognex.com>
2020-07-24 17:53:53 +0900

    2020-07-24 Listbox 추가 후 진행

--- Data_AugTool/Data_AugTool/MainWindow.xaml.cs
+++ Data_AugTool/Data_AugTool/MainWindow.xaml.cs
@@ -16,7 +16,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using OpenCvSharp;
-
+using Data_AugTool.Model;
+using OpenCvSharp.Extensions;
 
 namespace Data_AugTool
 {
@@ -25,10 +26,33 @@ namespace Data_AugTool
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
+
+        Augmentation augmentation = new Augmentation();
+        private List<AlbumentationInfo> AlbumentationInfos = new List<AlbumentationInfo>();
         public MainWindow()
         {
             InitializeComponent();
-
+            var contrast = new AlbumentationInfo()
+            {
+                IsChecked = false,
+                IsUseValueMax = true,
+                IsUseValueMin = true,
+                TypeName = "Contrast",
+                ValueMin = 0.0,
+                ValueMax = 1.0
+            };
+            var brightness = new AlbumentationInfo()
+            {
+                IsChecked = false,
+                IsUseValueMax = true,
+                IsUseValueMin = true,
+                TypeName = "Brightness",
+                ValueMin = -100.0,
+                ValueMax = 100.0
+            };
+            AlbumentationInfos.Add(contrast);
+            AlbumentationInfos.Add(brightness);
+            AlbumentationListBox.ItemsSource = AlbumentationInfos;
         }
 
         private void ListView1_SelectionChanged(object sender, SelectedCellsChangedEventArgs e)
@@ -75,6 +99,12 @@ namespace Data_AugTool
                 //Uri fileUri = new Uri(openFileDialog.FileName);
                 Dynamic2.Source = Test;
             }
+            double a = 0; ;
+            Mat mat = new Mat();
+            Mat outputMat;
+
+            outputMat = augmentation.GetRotationImage(mat, a);
+
 
         }
 
@@ -84,12 +114,48 @@ namespace Data_AugTool
 
             Dynamic2.Source = new BitmapImage(new Uri(selectedItem.ImageName));
 
-
+            UpdatePreviewImage();
         }
 
         private void CheckBox_Checked(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void AlbumentationListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdatePreviewImage();
+        }
+
+        private void UpdatePreviewImage()
+        {
+            var listbox = AlbumentationListBox as ListBox;
+            if (listbox == null)
+                return;
+
+            var listBoxSelectedItem = listbox.SelectedItem as AlbumentationInfo;
+
+            var selectedImageInfo = ListView1.SelectedItem as ImageInfo;
+            if (selectedImageInfo == null)
+                return;
+
+            //var originalImage = new BitmapImage(new Uri(selectedImageInfo.ImageName));
+            Mat orgMat = new Mat(selectedImageInfo.ImageName);
+            Mat previewMat = new Mat();
+
+            //ui_PreviewImage.Source = new BitmapImage(new Uri(selectedImageInfo.ImageName));
+            switch (listBoxSelectedItem.TypeName)
+            {
+                case "Brightness":
+                    Cv2.Add(orgMat, listBoxSelectedItem.ValueMin, previewMat);
+                    break;
+                case "Contrast":
+                    Cv2.AddWeighted(orgMat, listBoxSelectedItem.ValueMin, orgMat, 0, 0, previewMat);
+                    break;
+                default:
+                    break;
+            }
+            ui_PreviewImage.Source = previewMat.ToBitmapSource();
         }
     }
     public class ImageInfo
---
+++ Data_AugTool/Data_AugTool/Model/AlbumentationInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_AugTool.Model
+{
+    public class AlbumentationInfo
+    {
+        public string TypeName { get; set; }
+        public bool IsChecked { get; set; }
+        public double ValueMin { get; set; }
+        public double ValueMax { get; set; }
+        public bool IsUseValueMin { get; set; }
+        public bool IsUseValueMax { get; set; }
+    }
+}
