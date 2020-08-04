470d0d0a6055bf47b0722dd4e2cb2f8538abf0ae
NATICK-NT\nakim <Thomas.kim@cognex.com>
2020-08-04 13:22:59 +0900

    Zoom In 까지 작업한 내용 커밋 2020-08-04

--- Data_AugTool/Data_AugTool/MainWindow.xaml.cs
+++ Data_AugTool/Data_AugTool/MainWindow.xaml.cs
@@ -52,10 +52,8 @@ namespace Data_AugTool
                 new AlbumentationInfo("Zoom In"),
                 new AlbumentationInfo("Sharpen"),
                 new AlbumentationInfo("ColorWise"),
-                new AlbumentationInfo("Gradiation"),
+                new AlbumentationInfo("Gradation"),
                 new AlbumentationInfo("Distortion"),
-
-
 
             }
                 ); 
@@ -177,25 +175,47 @@ namespace Data_AugTool
                     Cv2.AddWeighted(orgMat, 1, matrix, 1, 0, previewMat);     //두 이미지를 가중치를 설정하여 합침
                     break;
                 case "Zoom In":
-                    Cv2.PyrDown(orgMat, previewMat);
+                    //Cv2.PyrDown(orgMat, previewMat);
 
+                    //#1. Center만 확대
+
+                    double width_param = (int)(0.8 * orgMat.Width); // 0.8이 배율 orgMat.Width이 원본이미지의 사이즈 // 나중에 0.8만 80%형식으로 바꿔서 파라미터로 빼야됨
+                    double height_param = (int)(0.8 * orgMat.Height); // 0.8이 배율 orgMat.Height 원본이미지의 사이즈 //
+
+                    int startX = orgMat.Width - (int)width_param;// 이미지를 Crop해올 좌상단 위치 지정하는값 // 원본사이즈 - 배율로 감소한 사이즈
+                    int startY = orgMat.Height - (int)height_param;//
+
+                    Mat tempMat = new Mat(orgMat, new OpenCvSharp.Rect(startX, startY, (int)width_param, (int)height_param));//중간과정 mat이고 Rect안에 x,y,width,height 값 지정해주는거
+                    //예외처리 범위 밖으로 벗어나는경우 shift시키거나 , 제로페딩을 시키거나
+                    //예외처리
+                    
+                    Cv2.Resize(tempMat, previewMat, new OpenCvSharp.Size(orgMat.Width, orgMat.Height),(double)((double)orgMat.Width/(double)width_param), (double)((double)orgMat.Height/(double)height_param), InterpolationFlags.Cubic);
+                    // (double) ( (double)orgMat.Width  /  (double)width_param)
+                    // 형변환       원본이미지 형변환      /       타겟이미지 배율    == 타겟이미지가 원본이미지 대비 몇배인가? 의 수식임
+                    // (double) ( (double)orgMat.Height  /  (double)height_param)
+
+                    //#2. 랜덤좌표
+                    //#3. 지정좌표
                     break;
 
                 case "Sharpen":
                     float[] data = new float[9] { -1, -1, -1, -1, 9, -1, -1, -1, -1 };
                     Mat kernel = new Mat(3, 3, MatType.CV_8S, data);
                     //Cv2.Filter2D(orgMat, previewMat, MatType.CV_8SC3, kernel, new OpenCvSharp.Point(0, 0));
-                    Cv2.AddWeighted(orgMat, 1.5, kernel, 00.5, 0, previewMat);
-                 
+                    Cv2.AddWeighted(orgMat, 1.5, kernel, 00.5, 0, previewMat);              
                     break;
+
                 case "ColorWise":
-                    Cv2.Add(orgMat, listBoxSelectedItem.ValueMin, previewMat);
+                    matrix = new Mat(orgMat.Size(), MatType.CV_8UC1);
+                    Cv2.CvtColor(orgMat, previewMat, ColorConversionCodes.BGR2GRAY);
                     break;
-                case "Gradiation":
-                    Cv2.Add(orgMat, listBoxSelectedItem.ValueMin, previewMat);
+
+                case "Gradation":
+                    
                     break;
+
                 case "Distortion":
-                    Cv2.Add(orgMat, listBoxSelectedItem.ValueMin, previewMat);
+                    
                     break;
 
                 default:
